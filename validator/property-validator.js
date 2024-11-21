1aa20c8dec1f91df95eb59b49c9ca0e5d948b6b8
khalid14414 <qwekukhalid266@gmail.com>
2024-11-21 16:48:28 +0000

    error debugging

--- validator/property-validator.js
+++ validator/property-validator.js
@@ -5,28 +5,16 @@ export const propertySchema = Joi.object({
     description: Joi.string().min(10).max(500).required(),
     location: Joi.string().min(3).max(100).required(),
     price: Joi.number().positive().required(),
-    category: Joi.string().valid('apartment','house','studio','shared room').required(),
+    category: Joi.string().valid('Apartment','House','Studio','Shared').required(),
     images: Joi.array().items(Joi.string()).min(1).required(), // Array of image URLs
-    status: Joi.string().valid('available', 'not available').default('available'),
-    features:Joi.object({
-        isFurnished: Joi.boolean().default(false),
-        isAirConditionerAvailable:Joi.boolean().default(false),
-        isParkingAvailable:Joi.boolean().default(false),
-    }).optional(),
-    amenities:Joi.array().items(Joi.string()).optional(),
+    status: Joi.string().valid('Available', 'Unavailable').default('Available'),
 });
 export const updatePropertySchema = Joi.object({
     title: Joi.string().min(3).max(100).required(),
     description: Joi.string().min(10).max(500).required(),
     location: Joi.string().min(3).max(100).required(),
     price: Joi.number().positive().required(),
-    category: Joi.string().valid('apartment','house','studio','shared room').required(),
+    category: Joi.string().valid('Apartment','House','Studio','Shared').required(),
     images: Joi.array().items(Joi.string()).optional(), //Array of image URLs
-    status: Joi.string().valid('available', 'not available').optional(),
-    features:Joi.object({
-        isFurnished: Joi.boolean().default(false),
-        isAirConditionerAvailable:Joi.boolean().default(false),
-        isParkingAvailable:Joi.boolean().default(false),
-    }).optional(),
-    amenities:Joi.array().items(Joi.string()).optional(),
+    status: Joi.string().valid('Available', 'Unavailable').optional(),
 })
